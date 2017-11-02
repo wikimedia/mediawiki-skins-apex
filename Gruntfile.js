c6eec6f0f85d3298760e06249dcf858228ae57e2
Umherirrender <umherirrender_de.wp@web.de>
2017-11-02 17:48:33 +0100

    build: Always exclude vendor
    Change-Id: I7bbac08ebbef7129bd81528ae1f0853f1a62271f

--- Gruntfile.js
+++ Gruntfile.js
@@ -12,7 +12,8 @@ module.exports = function ( grunt ) {
 		eslint: {
 			all: [
 				'**/*.js',
-				'!node_modules/**'
+				'!node_modules/**',
+				'!vendor/**'
 			]
 		},
 		stylelint: {
@@ -23,7 +24,8 @@ module.exports = function ( grunt ) {
 		jsonlint: {
 			all: [
 				'**/*.json',
-				'!node_modules/**'
+				'!node_modules/**',
+				'!vendor/**'
 			]
 		},
 		banana: conf.MessagesDirs,
@@ -32,7 +34,7 @@ module.exports = function ( grunt ) {
 				'<%= eslint.all %>',
 				'<%= stylelint.all %>',
 				'<%= jsonlint.all %>',
-				'.{stylelintrc}'
+				'.{stylelintrc,eslintrc}.json'
 			],
 			tasks: 'test'
 		}
